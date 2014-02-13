120b5e1f489405cf2955833f9616c4fe705e1ea6
Shailen Tuli <shailen.tuli@gmail.com>
2014-02-12 20:14:37 -0800

    Step 7 now uses a service

---
+++ web/7-service/piratebadge.dart
@@ -0,0 +1,65 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library ng_dart_codelab.piratebadge;
+
+import 'service/names_service.dart';
+import 'package:angular/angular.dart';
+import 'badge/badge_component.dart';
+
+@MirrorsUsed(override:'*')
+import 'dart:mirrors';
+
+class PirateName {
+  String firstName, appellation;
+
+  PirateName([this.firstName = '', this.appellation = '']);
+
+  String get pirateName => firstName.isEmpty ? '' :
+    '$firstName the $appellation';
+}
+
+@NgController(
+    selector: '[badges]',
+    publishAs: 'ctrl')
+class BadgesController {
+  NamesService ns; // Added.
+
+  bool dataLoaded = false;
+
+  BadgesController(this.ns);
+
+  PirateName pn;
+
+  String _name = '';
+
+  get name => _name;
+
+  set name(newName) {
+    _name = newName;
+    ns.randomAppellation().then((appellation) {
+      pn = new PirateName(name, appellation);
+    });
+  }
+
+  bool get inputIsNotEmpty => name.trim().isNotEmpty;
+
+  String get label => inputIsNotEmpty ? "Arrr! Write yer name!" :
+    "Aye! Gimme a name!";
+
+  generateName() {
+    pn = new PirateName();
+    return ns.randomAppellation()
+        .then((appellation) => pn.appellation = appellation)
+        .then((_) => ns.randomName())
+        .then((name) => pn.firstName = name);
+  }
+}
+
+void main() {
+  ngBootstrap(module: new Module()
+    ..type(BadgesController)
+    ..type(NamesService) // Added.
+    ..type(BadgeComponent));
+}
---
+++ web/7-service/service/names_service.dart
@@ -0,0 +1,34 @@
+library ng_dart_codelab.names_service;
+
+import 'package:angular/angular.dart';
+import 'dart:async';
+import 'dart:math';
+
+@NgInjectableService()
+class NamesService {
+  static final Random indexGen = new Random();
+  final Http _http;
+  List<String> names;
+  List<String> appellations;
+
+  NamesService(this._http);
+
+  Future _load() {
+    if (names != null) return new Future.value(true);
+    return _http.get('piratenames.json').then((HttpResponse response) {
+      print('getting data.....');
+      names = response.data['names'];
+      appellations = response.data['appellations'];
+    });
+  }
+
+  Future<String> randomName() {
+    return _load().then((_) => _oneRandom(names));
+  }
+
+  Future<String> randomAppellation() {
+    return _load().then((_) => _oneRandom(appellations));
+  }
+
+  String _oneRandom(List<String> list) => list[indexGen.nextInt(list.length)];
+}
