172bc8ebcc47ebbbb7fb3b44498e115a4d8aff86
Shailen Tuli <shailen.tuli@gmail.com>
2014-02-13 15:59:08 -0800

    Removed comments

--- web/5-classbadge/piratebadge.dart
+++ web/5-classbadge/piratebadge.dart
@@ -10,7 +10,6 @@ import 'badge/badge_component.dart';
 @MirrorsUsed(override:'*')
 import 'dart:mirrors';
 
-// 1) add a model
 import 'dart:math' show Random;
 
 class PirateName {
@@ -27,7 +26,6 @@ class PirateName {
     publishAs: 'ctrl')
 class BadgesController {
 
-  // 3) define some data
   static const List names = const [
     'Anne', 'Mary', 'Jack', 'Morgan', 'Roger',
     'Bill', 'Ragnar', 'Ed', 'John', 'Jane' ];
@@ -36,21 +34,12 @@ class BadgesController {
     'Black','Damned', 'Jackal', 'Red', 'Stalwart', 'Axe',
     'Young', 'Old', 'Angry', 'Brave', 'Crazy', 'Noble'];
 
-  // 2) Create a variable for the model data.
   PirateName pn;
 
-
-  // 4) Create a variable that binds to the UI and helps us set the
-  // firstName and the appellation for the pirate.
-  // This is a common pattern. Define a private variable and implement
-  // a getter and a setter to get and set the value of that variable.
   String _name = '';
 
   get name => _name;
 
-  // 4) The setter not only sets the value of _name, it also set the
-  // firstName and appelation fields of the PirateBadge object. The
-  //
   set name(newName) {
     _name = newName;
     pn = new PirateName(name,
@@ -62,9 +51,6 @@ class BadgesController {
   String get label => inputIsNotEmpty ? "Arrr! Write yer name!" :
     "Aye! Gimme a name!";
 
-
-  // 5) Finally, implement code to generate a pirate name with a randomly
-  // chosen firstName and appellation.
   generateName() {
     var rand = new Random();
     var randomName = names[rand.nextInt(names.length)];
